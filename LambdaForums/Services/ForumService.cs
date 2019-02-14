9f569c4d8ac708c93714dce9c5182739c1084607
yachnytskyi <yachnytskyi1992@gmail.com>
2019-02-14 02:18:08 -0800

    Add service layer Data/Interfaces Iforum and Services/ ForumService (for Forum)

---
+++ LambdaForums/Services/ForumService.cs
@@ -0,0 +1,57 @@
+﻿using LambdaForums.Data;
+using LambdaForums.Data.Interfaces;
+using LambdaForums.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LambdaForums.Services
+{
+    public class ForumService : IForum
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ForumService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task Create(Forum forum)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(int forumId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Forum> GetAll()
+        {
+            return _context.Forums
+                .Include(forum => forum.Posts);
+        }
+
+        public IEnumerable<ApplicationUser> GetAllActiveUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Forum GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateForumDescription(int forumId, string newDescription)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateForumTitle(int forumId, string newTitle)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
