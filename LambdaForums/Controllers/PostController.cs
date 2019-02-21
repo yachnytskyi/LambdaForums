4efc750447bc3a9342399d8329204ccafe33c88b
yachnytskyi <yachnytskyi1992@gmail.com>
2019-02-21 02:56:45 -0800

    Add Controllers/ PostController, Models/Post/ PostIndexModel, Models/Reply/ PostReplyModel and fixed Startup.cs

---
+++ LambdaForums/Controllers/PostController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using LambdaForums.Data.Interfaces;
+using LambdaForums.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LambdaForums.Controllers
+{
+    public class PostController : Controller
+    {
+        private readonly IPost _postService;
+
+        public PostController(IPost postService)
+        {
+            _postService = postService;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index(int id)
+        {
+            var post = _postService.GetById(id);
+
+            var replies = BuildPostReplies(post.Replies);
+
+            var model = new PostIndexModel
+            {
+                Id = post.Id,
+                Title = post.Title,
+                AuthorId = post.User.Id,
+                AuthorName = post.User.UserName,
+                AuthorImageUrl = post.User.ProfileImageUrl,
+                AuthorRating = post.User.Rating,
+                Created = post.Created,
+                PostContent = post.Content,
+                Replies = replies
+            };
+
+            return View(model);
+        }
+
+        private IEnumerable<PostReplyModel> BuildPostReplies(IEnumerable<PostReply> replies)
+        {
+            return replies.Select(reply => new PostReplyModel
+            {
+                Id = reply.Id,
+                AuthorName = reply.User.UserName,
+                AuthorId = reply.User.Id,
+                AuthorImageUrl = reply.User.ProfileImageUrl,
+                AuthorRating = reply.User.Rating,
+                Created = reply.Created,
+                ReplyContent = reply.Content
+            });
+        }
+    }
+}
