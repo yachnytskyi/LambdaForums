de877cc781fd263a2fdea0718d21313fb766f84d
yachnytskyi <yachnytskyi1992@gmail.com>
2019-02-21 01:53:47 -0800

    Add Models/Forum/ ForumTopicModel and fixed Controllers/ ForumController

--- LambdaForums/Controllers/ForumController.cs
+++ LambdaForums/Controllers/ForumController.cs
@@ -39,7 +39,7 @@ namespace LambdaForums.Controllers
         public IActionResult Topic(int id)
         {
             var forum = _forumService.GetById(id);
-            var posts = _postService.GetPostsByForumId(id);
+            var posts = forum.Posts;
 
             var postListings = posts.Select(post => new PostListingModel {
                 Id = post.Id,
@@ -51,14 +51,25 @@ namespace LambdaForums.Controllers
                 Forum = BuildForumListing(post)
             });
 
-            return View();
+            var model = new ForumTopicModel
+            {
+                Posts = postListings,
+                Forum = BuildForumListing(forum)
+
+            };
+
+            return View(model);
 
         }
 
         private ForumListingModel BuildForumListing(Post post)
         {
             var forum = post.Forum;
+            return BuildForumListing(forum);
+        }
 
+        private ForumListingModel BuildForumListing(Forum forum)
+        {
             return new ForumListingModel
             {
                 Id = forum.Id,
@@ -66,6 +77,6 @@ namespace LambdaForums.Controllers
                 Description = forum.Description,
                 ForumImageUrl = forum.ImageUrl
             };
-        }
+        }    
     }
 }
---
+++ LambdaForums/Models/Forum/ForumTopicModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+
+namespace LambdaForums.Models
+{
+    public class ForumTopicModel
+    {
+        public ForumListingModel Forum { get; set; }
+        public IEnumerable<PostListingModel> Posts { get; set; }
+    }
+}
