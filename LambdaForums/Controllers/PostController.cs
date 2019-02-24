2e528d8f518625d88edfeb85e1444bb84a85aed5
yachnytskyi <yachnytskyi1992@gmail.com>
2019-02-24 01:30:47 -0800

    Add Views/Post Create View, fix Controllers/ PostController, Services/PostService and Views/Forum/ Topic View

--- LambdaForums/Controllers/PostController.cs
+++ LambdaForums/Controllers/PostController.cs
@@ -70,24 +70,27 @@ namespace LambdaForums.Controllers
         public async Task<IActionResult> AddPost(NewPostModel model)
         {
             var userId = _userManager.GetUserId(User);
-            var user = await _userManager.FindByIdAsync(userId);
+            var user = _userManager.FindByIdAsync(userId).Result;
             var post = BuildPost(model, user);
 
             await _postService.Add(post);
 
             // Todo: Implement User Rating Management
 
-            return RedirectToAction("Index", "Post", post.Id);
+            return RedirectToAction("Index", "Post", new { id = post.Id });
         }
 
         private Post BuildPost(NewPostModel model, ApplicationUser user)
         {
+            var forum = _forumService.GetById(model.ForumId);
+
             return new Post
             {
                 Title = model.Title,
                 Content = model.Content,
                 Created = DateTime.Now,
-                User = user
+                User = user,
+                Forum = forum
             };
         }
 
