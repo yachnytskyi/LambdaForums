18952e450d83fc1e8f943b4e642ca5455e12c6a0
Yachnytskyi <yachnytskyi1992@gmail.com>
2019-09-02 09:40:27 +0300

    Implemented new features Create and Delete for Forum, fixed ForumService and Views Forum Create

--- LambdaForums/Services/ForumService.cs
+++ LambdaForums/Services/ForumService.cs
@@ -18,14 +18,17 @@ namespace LambdaForums.Services
             _context = context;
         }
 
-        public Task Create(Forum forum)
+        public async Task Create(Forum forum)
         {
-            throw new NotImplementedException();
+            _context.Add(forum);
+            await _context.SaveChangesAsync();
         }
 
-        public Task Delete(int forumId)
+        public async Task Delete(int forumId)
         {
-            throw new NotImplementedException();
+            var forum = GetById(forumId);
+            _context.Remove(forum);
+            await _context.SaveChangesAsync();
         }
 
         public IEnumerable<Forum> GetAll()
